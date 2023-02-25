1424846c2d67caff3d39b1b6f231065d9cd8241e
npolley <njpolley888@gmail.com>
2023-02-25 15:45:05 -0500

    Finished project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,53 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    
+    const get_book = new Promise((resolve, reject) => {
+        resolve(res.send(books[isbn]));
+      });
+
+      get_book.then(() => console.log("Promise for Task 11 resolved"));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const writer = req.params.author;
+    const results = Object.values(books).filter(({author}) => author === writer);
+    
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(res.send(results)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 12 resolved"));
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const cover = req.params.title;
+    const results = Object.values(books).filter(({title}) => title === cover);
+    
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(res.send(results)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 13 resolved"));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
